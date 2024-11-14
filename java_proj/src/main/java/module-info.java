f19c885f84fb5bd098fca8fc0d1c2a3f4964eca2
Fednamedan <99498289+Fednamedan@users.noreply.github.com>
2024-11-14 08:51:45 +0300

    Delete java_proj directory

--- java_proj/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.kurs {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.kurs to javafx.fxml;
-    exports com.example.kurs;
-}
